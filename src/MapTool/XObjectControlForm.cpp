6a7eab8e3683c8e34c6e6dc2ae01b14f47397c68
Seung-Hoon Kuk <shkuk88@Gmail.com>
2018-12-24 17:02:42 +0900

    add object button

--- src/MapTool/XObjectControlForm.cpp
+++ src/MapTool/XObjectControlForm.cpp
@@ -30,11 +30,53 @@ XObjectControlForm::~XObjectControlForm()
 void XObjectControlForm::DoDataExchange(CDataExchange* pDX)
 {
 	CFormView::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_OBJ_LIST, m_ObjectList);
 }
 
 
 BEGIN_MESSAGE_MAP(XObjectControlForm, CFormView)
+	ON_BN_CLICKED(IDC_ADD_OBJECT, &XObjectControlForm::OnBnClickedAddObject)
+	ON_BN_CLICKED(IDC_DEL_OBJECT, &XObjectControlForm::OnBnClickedDelObject)
 END_MESSAGE_MAP()
 
 
 // XObjectControlForm 메시지 처리기
+
+
+void XObjectControlForm::OnBnClickedAddObject()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	TCHAR fileFilter[] = _T("Object (*.shk) | *.SHK; | All Files(*.*)|*.*||");
+	CFileDialog dlg(TRUE, NULL, NULL, OFN_HIDEREADONLY, fileFilter);
+
+	if (dlg.DoModal() == IDOK)
+	{
+		for (int iObj = 0; iObj < m_szObjectPath.size(); iObj++)
+		{
+			if (!m_szObjectPath[iObj].Compare(dlg.GetPathName()))	// 같은 Texture면 그냥 return
+			{
+				return;
+			}
+		}
+		m_szObjectPath.push_back(dlg.GetPathName());
+	}
+	else // 확인 외에 다른 버튼을 누를시 리턴
+	{
+		return;
+	}
+	m_iSelectObject = m_szObjectPath.size() - 1;
+	// 경로 추출한 문자열이 리스트에 보이도록 한다. 경로가 포함 문자열은 따로 가지고 있는다. 
+	CString szDelPath = m_szObjectPath[m_iSelectObject].Right(m_szObjectPath[m_iSelectObject].GetLength() - m_szObjectPath[m_iSelectObject].ReverseFind('\\') - 1);
+	m_ObjectList.InsertString(m_iSelectObject, szDelPath);
+}
+
+
+void XObjectControlForm::OnBnClickedDelObject()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	m_iSelectObject = m_ObjectList.GetCurSel();
+	if (m_iSelectObject == -1)	return;
+	m_ObjectList.DeleteString(m_iSelectObject);
+
+	m_szObjectPath.erase(m_szObjectPath.begin() + m_iSelectObject);
+}
--- src/MapTool/XObjectControlForm.h
+++ src/MapTool/XObjectControlForm.h
@@ -6,7 +6,13 @@
 class XObjectControlForm : public CFormView
 {
 	DECLARE_DYNAMIC(XObjectControlForm)
-	// 사용자 정의 함수
+
+//사용자 정의 변수
+private:
+	vector<CString> m_szObjectPath;
+	int m_iSelectObject;
+	
+// 사용자 정의 함수
 public:
 	static XObjectControlForm* CreateOne(CWnd* pParent);
 public:
@@ -15,11 +21,15 @@ public:
 
 // 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_XObjectControlForm };
+	enum { IDD = IDD_OBJECT_FORM};
 #endif
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
 
 	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedAddObject();
+	afx_msg void OnBnClickedDelObject();
+	CListBox m_ObjectList;
 };
